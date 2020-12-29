80ee2d29359595da3a01b545e65fc1d5910975e9
ukiyoyo <13175394+ukiyoyo@users.noreply.github.com>
2020-12-29 13:04:27 +0900

    Print directly

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().len() != 2 {
+        eprintln!("Invalid argments number: {}", env::args().len());
+        return;
+    }
+
+    println!(".intel_syntax noprefix");
+    println!(".globl main");
+    println!("main:");
+    println!("  mov rax, {}", env::args().nth(1).unwrap());
+    println!("  ret");
+
+    return;
 }
