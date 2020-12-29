41d3fd8e2776ebf5391603cf0a5c3f1cb7c59bb8
ukiyoyo <13175394+ukiyoyo@users.noreply.github.com>
2020-12-29 22:57:25 +0900

    Add tokenizer

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,103 @@
-use std::{env, unimplemented};
+use std::collections::VecDeque;
+use std::env;
 
-fn search_split_idx(s: &str) -> Option<usize> {
-    s.find(|c: char| !c.is_ascii_digit())
+fn split_digit(s: &str) -> usize {
+    s.find(|c: char| !c.is_ascii_digit()).unwrap_or(s.len())
+}
+
+#[derive(PartialEq, Debug)]
+enum TokenKind {
+    Reserved,
+    NUM,
+    EOF,
+}
+
+#[derive(Debug)]
+struct Token {
+    kind: TokenKind,
+    string: String,
+}
+
+struct Tokenizer {
+    tokens: VecDeque<Token>,
+}
+
+impl Tokenizer {
+    fn new() -> Self {
+        Tokenizer {
+            tokens: VecDeque::new(),
+        }
+    }
+
+    fn new_token(&mut self, kind: TokenKind, string: String) {
+        self.tokens.push_back(Token { kind, string });
+    }
+
+    fn tokenize(&mut self, input: String) {
+        self.tokens.clear();
+        let mut remain = input.as_str();
+        while let Some(next) = remain.chars().nth(0) {
+            match next {
+                ' ' => {
+                    remain = remain.trim();
+                }
+                '+' | '-' => {
+                    self.new_token(TokenKind::Reserved, next.to_string());
+                    let (_, t) = remain.split_at(1);
+                    remain = t;
+                }
+                _ if next.is_ascii_digit() => {
+                    let idx = split_digit(remain);
+                    let (s1, s2) = remain.split_at(idx);
+                    self.new_token(TokenKind::NUM, s1.to_string());
+                    remain = s2;
+                }
+                _ => panic!("Tokenizer error: invalid character {}", next),
+            }
+        }
+        self.new_token(TokenKind::EOF, '\0'.to_string());
+    }
+
+    fn consume(&mut self, op: char) -> bool {
+        if let Some(token) = self.tokens.front() {
+            if token.kind != TokenKind::Reserved || !token.string.starts_with(op) {
+                return false;
+            }
+            self.tokens.pop_front();
+            return true;
+        } else {
+            panic!("Tokenizer consume() error: tokens don't exist");
+        }
+    }
+
+    fn expect(&mut self, op: char) {
+        if let Some(token) = self.tokens.pop_front() {
+            if token.kind != TokenKind::Reserved || !token.string.starts_with(op) {
+                panic!("Tokenizer expect() error: not {}", op)
+            }
+        } else {
+            panic!("Tokenizer expect() error: tokens don't exist");
+        }
+    }
+
+    fn expect_number(&mut self) -> i64 {
+        if let Some(token) = self.tokens.pop_front() {
+            if token.kind != TokenKind::NUM {
+                panic!("Tokenizer expect_number() error: not numbers");
+            }
+            return token.string.parse().unwrap();
+        } else {
+            panic!("Tokenizer expect_number() error: tokens don't exist");
+        }
+    }
+
+    fn at_eof(&self) -> bool {
+        if let Some(token) = self.tokens.front() {
+            token.kind == TokenKind::EOF
+        } else {
+            false
+        }
+    }
 }
 
 fn main() {
@@ -12,31 +108,24 @@ fn main() {
 
     let input = env::args().nth(1).unwrap();
 
+    let mut tokenizer = Tokenizer::new();
+    tokenizer.tokenize(input);
+
+    dbg!(&tokenizer.tokens);
+
     println!(".intel_syntax noprefix");
     println!(".globl main");
     println!("main:");
 
-    let idx = search_split_idx(input.as_str()).unwrap_or(input.len());
-    let (d, mut remain) = input.split_at(idx);
-    println!("  mov rax, {}", d);
-
-    while let Some(c) = remain.chars().nth(0) {
-        remain = &remain[1..];
-        let idx = search_split_idx(remain).unwrap_or(remain.len());
-        let s = remain.split_at(idx);
-        match c {
-            '+' => {
-                println!("  add rax, {}", s.0);
-                remain = s.1;
-            }
-            '-' => {
-                println!("  sub rax, {}", s.0);
-                remain = s.1;
-            }
-            _ => unimplemented!(),
+    println!("  mov rax, {}", tokenizer.expect_number());
+    while !tokenizer.at_eof() {
+        if tokenizer.consume('+') {
+            println!("  add rax, {}", tokenizer.expect_number());
+        } else {
+            tokenizer.expect('-');
+            println!("  sub rax, {}", tokenizer.expect_number());
         }
     }
-
     println!("  ret");
     return;
 }
